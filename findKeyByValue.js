cc044dc4b30310f9c40b7ccd6993f307026e870a
Difan <dfhou29@gmail.com>
2023-07-26 21:26:41 -0700

    findKeyByValue.js added.

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// const findKeyByValueV2 = function(object, value) {
+//   const keys = Object.keys(object);
+
+//   for (let key of keys) {
+//     if (object[key] === value) {
+//       return key;
+//     }
+//   }
+//   return undefined;
+// };
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
